65b1cbfcd477b8462e70a7e88374d00215759433
Behzad Sharafi <behzadsharafi@gmail.com>
2024-02-05 12:18:02 +1100

    adds a logo to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import { SiAdventofcode } from "react-icons/si";
 
 const NavBar = () => {
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <SiAdventofcode />
+      </Link>
       <ul className="flex space-x-6">
         <li>
           <Link href="/">Dashboard</Link>
