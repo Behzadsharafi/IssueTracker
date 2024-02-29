a423507aadf8e58f9e07d32c3cd31fad5ef1df1b
Behzad Sharafi <behzadsharafi@gmail.com>
2024-02-29 20:48:44 +1100

    builds the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
   return (
-    <div>Hello World</div>
-  )
+    <Pagination itemCount={30} pageSize={10} currentPage={1} />
+  );
 }
